f1f6ff60252bd03d86cf6f73b04bddafbc7a8aa5
ChimpsterMan <emnilsson007@gmail.com>
2021-08-24 21:21:25 -0400

    More small tweaks to try and fix the advect shader bug

--- IncompressibleFluidSimulation/resources/shaders/advect.fs
+++ IncompressibleFluidSimulation/resources/shaders/advect.fs
@@ -18,7 +18,7 @@ float floorTo(float num, float inc){
 }
 
 vec4 advect(sampler2D v, sampler2D d, vec2 coords, float dt, float rdx) {
-	vec2 pos = coords - dt * vec2(texture(v, coords));
+	vec2 pos = coords - dt * rdx * 0 * vec2(texture(v, coords));
 
 	// weight each neighboring texel based on how close it is to the downstream position
 	float i0 = floorTo(pos.x, rdx);
@@ -36,7 +36,7 @@ vec4 advect(sampler2D v, sampler2D d, vec2 coords, float dt, float rdx) {
 	vec2 i1j0 = vec2(i1, j0);
 	vec2 i1j1 = vec2(i1, j1);
 
-	//return vec4(abs(vec2(texture(d, pos))) * 0.5, 0.0, 1.0);
+	return texture(d, TexCoords);
 
 	return 
 		s0 * (t0 * texture(d, i0j0) + t1 * texture(d, i0j1)) +
@@ -45,4 +45,5 @@ vec4 advect(sampler2D v, sampler2D d, vec2 coords, float dt, float rdx) {
 
 void main(){
 	FragColor = vec4(vec3(advect(v, d, TexCoords, dt, rdx)), 1.0);
+	FragColor = vec4(vec3(texture(d, TexCoords)), 1.0);
 }
